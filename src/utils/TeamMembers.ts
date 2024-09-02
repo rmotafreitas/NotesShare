d1a2252ccf91fd655f7a49810dafc1e20131b288
Ness.js <68559739+rmotafreitas@users.noreply.github.com>
2024-09-02 18:42:54 +0100

    Update TeamMembers.ts

--- src/utils/TeamMembers.ts
+++ src/utils/TeamMembers.ts
@@ -7,48 +7,6 @@ import {
 
 export const TEAM_MEMBERS: any[] = [
   {
-    name: "Afonso Oliveira",
-    role: {
-      pt: "Desenvolvedor",
-      en: "Developer",
-    },
-    photo: AfonsoOliveira,
-    age: 18,
-    mail: "1221160@isep.ipp.pt",
-    hobby: {
-      pt: "Ler 📖",
-      en: "Reading 📖",
-    },
-  },
-  {
-    name: "Diogo Sousa",
-    role: {
-      pt: "Desenvolvedor",
-      en: "Developer",
-    },
-    photo: DiogoSousa,
-    age: 22,
-    mail: "1181024@isep.ipp.pt",
-    hobby: {
-      pt: "Praticar Surf 🏄‍♂️",
-      en: "Surfing 🏄‍♂️",
-    },
-  },
-  {
-    name: "Pedro Vieira",
-    role: {
-      pt: "Desenvolvedor",
-      en: "Developer",
-    },
-    photo: PedroViera,
-    age: 18,
-    mail: "1221415@isep.ipp.pt",
-    hobby: {
-      pt: "Jogar Badminton 🏸",
-      en: "Playing Badminton 🏸",
-    },
-  },
-  {
     name: "Ricardo Freitas",
     role: {
       pt: "Desenvolvedor",
